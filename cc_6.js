81a0d349450ab4a52c78a611c3eeaa3e5ecc3ef6
Fotihjon <ikromov@usf.edu>
2025-02-09 19:34:52 -0500

    Task 2: Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,9 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 console.log(calculateProfit(20, 30, 100)); // "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200)); // "Total Profit: $4000"
 
+// Task 2: Sales Tax Computation
+const calculateSalesTax = function(amount, taxRate) {
+    return `Sales Tax: $${amount * taxRate}`;
+};
+console.log(calculateSalesTax(100, 0.07)); // "Sales Tax: $7"
+console.log(calculateSalesTax(500, 0.1));  // "Sales Tax: $50"
